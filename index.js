1aa7a0ea40bf8f14b7e614df85f569cbf880c0c9
tamunosakijonah <tamunosakijonah@gmail.com>
2022-06-24 14:08:54 +0100

    javascrit_task

---
+++ index.js
@@ -0,0 +1,23 @@
+/*function getFullName(firstname,lastname){
+    let fullName;
+    fullName = firstname + ' ' + lastname;
+    return fullName;
+}
+let fullName = getFullName('Tamunosaki','Jonah');
+alert(fullName);
+
+function getHeight(digit,measurement){
+    let height;
+    height = digit + ' ' + measurement;
+    return height;
+}
+let height = getHeight('4.65','ft');
+alert(height)*/
+
+
+let myName = "Tamunosaki jonah"
+console.log('my name is:', myName)
+let myHeight="4.7 ft"
+console.log('i am', myHeight)
+let myCountry=" Nigeria"
+console.log('i am from',myCountry)
