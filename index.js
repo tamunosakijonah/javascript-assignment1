4ab706c1ead2450ef9a3f66b4f2518f94856edb6
tamunosakijonah <tamunosakijonah@gmail.com>
2022-07-06 22:46:41 +0100

    modified

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-/*function getFullName(firstname,lastname){
-    let fullName;
-    fullName = firstname + ' ' + lastname;
-    return fullName;
-}
-let fullName = getFullName('Tamunosaki','Jonah');
-alert(fullName);
-
-function getHeight(digit,measurement){
-    let height;
-    height = digit + ' ' + measurement;
-    return height;
-}
-let height = getHeight('4.65','ft');
-alert(height)*/
-
-
-let myName = "Tamunosaki jonah"
-console.log('my name is:', myName)
-let myHeight="4.7 ft"
-console.log('i am', myHeight)
-let myCountry=" Nigeria"
-console.log('i am from',myCountry)
+let myName = prompt("enter name");
+let myHeight=prompt("enter height");
+let myCountry=prompt("enter country");
+document.write("My name is" + " " + myName + ", " + "I am" + " " + myHeight + " " + "feets tall," + " " + "I am from" +" " + myCountry)
